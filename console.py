1a6e8f60e8ceb482a72e7d1b89ba53074bf53e41
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2024-01-14 11:36:06 +0200

    12. Count instances

--- console.py
+++ console.py
@@ -49,7 +49,8 @@ class HBNBCommand(cmd.Cmd):
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
-            "update": self.do_update
+            "update": self.do_update,
+            "count": self.do_count
         }
         # Check if the input contains a dot (.)
         match = re.search(r"\.", arg)
@@ -196,6 +197,16 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     obj.__dict__[k] = v
         storage.save()
+
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
 
 if __name__ == '__main__':
